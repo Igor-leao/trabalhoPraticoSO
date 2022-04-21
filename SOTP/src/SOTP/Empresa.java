446b50e9c2f7fd920e5c633862cd7cbd8075e6c1
Administrador <Administrador@igor-leao>
2022-04-21 15:11:36 -0300

    Trabalho pratico Sistemas Operacionais

---
+++ SOTP/src/SOTP/Empresa.java
@@ -0,0 +1,10 @@
+package SOTP;
+
+public class Empresa {
+
+	public static void main(String[] args) {
+//Aqui onde vão ser criados os metodos para cada chamada, não precisa criar aqui não
+//Só importar o método
+	}
+
+}
